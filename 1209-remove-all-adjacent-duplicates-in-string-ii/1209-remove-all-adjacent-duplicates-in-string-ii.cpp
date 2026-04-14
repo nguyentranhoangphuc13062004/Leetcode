d340c4eede088fa7413b19b5ae75455629684d32
nguyentranhoangphuc13062004 <135836850+nguyentranhoangphuc13062004@users.noreply.github.com>
2026-04-14 07:28:47 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string removeDuplicates(string s, int k) {
+        vector<pair<char , int>> st;
+
+        for (char c: s){
+            if (!st.empty()  && st.back().first == c){
+                    st.back().second++;
+            if (st.back().second == k){
+                st.pop_back();
+              }
+            }
+           else {
+            st.push_back({c , 1});
+           } 
+
+        }
+
+         string ketqua ="";
+         for (auto p: st){
+            ketqua.append(p.second , p.first);
+         }
+
+         return ketqua;
+    }
+};
